07a80474a2838cc71ad5685bbb3fc842d8afb2f3
douglas <douglasrm.azevedo@gmail.com>
2020-03-07 17:04:53 -0300

    Changing format r2d3.R call

--- R/r2d3.R
+++ R/r2d3.R
@@ -1,14 +1,14 @@
 ##-- Packages 
+library(dplyr)
 library(r2d3)
 
 ##--
 data_tst <- read.table(file = "data/brand_values.csv", sep = ",", dec = ".", header = TRUE, quote = "\"")
-options <- list(title = "Acusto PNC", subtitle = "Almos Done!", caption = "Caption aqui!",
-                tickDuration = 500, 
-                top_n = 12, 
+options <- list(title = "Acusto PNC", subtitle = "Almost Done!", caption = "Caption aqui!",
+                first_year = 2000, last_year = 2018,
+                top_n = 12, tick_duration = 500,
                 height = 600, width = 960,
-                margin_top = 80, margin_right = 0, margin_bottom = 5, margin_left = 100,
-                first_year = 2000, last_year = 2018)
+                margin_top = 80, margin_right = 0, margin_bottom = 5, margin_left = 0)
 
 r2d3(data = data_tst, 
      css = "www/styles.css", 
