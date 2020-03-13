fef1b53df56bfb179215c0767dff8e0f69d90ae9
douglas <douglasrm.azevedo@gmail.com>
2020-03-13 18:44:51 -0300

    Changing barchart race js structure

--- shiny/www/js/barchartrace2.js
+++ shiny/www/js/barchartrace2.js
@@ -4,25 +4,25 @@ r2d3.onRender(function(data, svg, width, height, options) {
   svg.selectAll("g").remove();
   svg.selectAll("rect.bar").remove();
   svg.selectAll("text.label").remove();
-  svg.selectAll("text.valueLabel").remove();
-  svg.selectAll("frameText").remove();
+  svg.selectAll("text.value_label").remove();
+  svg.selectAll("frame_text").remove();
   
   // Parameters
   var tick_duration = options.tick_duration;
   var top_n = options.top_n;
-    
+
   const margin = {
     top: options.margin_top,
     right: options.margin_right,
     bottom: options.margin_bottom,
     left: options.margin_left
   };
-
+  
   let bar_padding = (height-(margin.bottom+margin.top))/(top_n*5);
 
   let frame = options.first_frame;
   let last_frame = options.last_frame;
-
+  
   // Text information
   let title = svg.append('text')
     .attr('class', 'title')
@@ -47,18 +47,20 @@ r2d3.onRender(function(data, svg, width, height, options) {
     d.last_value = +d.last_value,
     d.value = isNaN(d.value) ? 0 : d.value,
     d.frame = +d.frame,
+    d.frame_label = +d.frame_label,
     //d.colour = d3.hsl(d.colour);
     d.colour = d3.hsl(Math.random()*360,0.75,0.75);
   });
-    
-  let frameSlice = data.filter(d => d.frame == frame && !isNaN(d.value))
+  
+  let frame_slice = data.filter(d => d.frame == frame && !isNaN(d.value))
     .sort((a,b) => b.value - a.value)
     .slice(0, top_n);
   
-  frameSlice.forEach((d,i) => d.rank = i);
+  frame_slice.forEach((d, i) => d.rank = i);
   
+  // Axis
   let x = d3.scaleLinear()
-    .domain([0, d3.max(frameSlice, d => d.value)])
+    .domain([0, d3.max(frame_slice, d => d.value)])
     .range([margin.left, width-margin.right-65]);
   
   let y = d3.scaleLinear()
@@ -79,7 +81,7 @@ r2d3.onRender(function(data, svg, width, height, options) {
     .classed('origin', d => d === 0);
   
   svg.selectAll('rect.bar')
-    .data(frameSlice, d => d.name)
+    .data(frame_slice, d => d.name)
     .enter()
     .append('rect')
     .attr('class', 'bar')
@@ -90,7 +92,7 @@ r2d3.onRender(function(data, svg, width, height, options) {
     .style('fill', d => d.colour);
       
   svg.selectAll('text.label')
-    .data(frameSlice, d => d.name)
+    .data(frame_slice, d => d.name)
     .enter()
     .append('text')
     .attr('class', 'label')
@@ -99,31 +101,38 @@ r2d3.onRender(function(data, svg, width, height, options) {
     .style('text-anchor', 'end')
     .html(d => d.name);
       
-  svg.selectAll('text.valueLabel')
-    .data(frameSlice, d => d.name)
+  svg.selectAll('text.value_label')
+    .data(frame_slice, d => d.name)
     .enter()
     .append('text')
-    .attr('class', 'valueLabel')
+    .attr('class', 'value_label')
     .attr('x', d => x(d.value)+5)
     .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1)
     .text(d => d3.format(',.0f')(d.last_value));
 
-  let frameText = svg.append('text')
-    .attr('class', 'frameText')
+  let frame_text = svg.append('text')
+    .attr('class', 'frame_text')
     .attr('x', width-margin.right)
     .attr('y', height-25)
     .style('text-anchor', 'end')
-    .html(~~frame)
+    //.html(~~frame)
     .call(halo, 10);
+    
+  function update_frame_lbl(){
+    var lbl = options.frame_labels.slice(frame - 1, frame);
+    frame_text.html(`${lbl}`);
+  }
+  
+  update_frame_lbl();
      
   let ticker = d3.interval(e => {
     
-    frameSlice = data.filter(d => d.frame == frame && !isNaN(d.value))
+    frame_slice = data.filter(d => d.frame == frame && !isNaN(d.value))
       .sort((a,b) => b.value - a.value)
-      .slice(0,top_n);
+      .slice(0, top_n);
 
-    frameSlice.forEach((d,i) => d.rank = i);
-    x.domain([0, d3.max(frameSlice, d => d.value)]); 
+    frame_slice.forEach((d,i) => d.rank = i);
+    x.domain([0, d3.max(frame_slice, d => d.value)]); 
      
     svg.select('.xAxis')
       .transition()
@@ -131,114 +140,115 @@ r2d3.onRender(function(data, svg, width, height, options) {
         .ease(d3.easeLinear)
         .call(xAxis);
     
-     let bars = svg.selectAll('.bar').data(frameSlice, d => d.name);
+    let bars = svg.selectAll('.bar').data(frame_slice, d => d.name);
     
-     bars
-       .enter()
-       .append('rect')
-       .attr('class', d => `bar ${d.name.replace(/\s/g,'_')}`)
-       .attr('x', x(0)+1)
-       .attr( 'width', d => x(d.value)-x(0)-1)
-       .attr('y', d => y(top_n+1)+5)
-       .attr('height', y(1)-y(0)-bar_padding)
-       .style('fill', d => d.colour)
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('y', d => y(d.rank)+5);
+    bars
+      .enter()
+      .append('rect')
+      .attr('class', d => `bar ${d.name.replace(/\s/g,'_')}`)
+      .attr('x', x(0)+1)
+      .attr( 'width', d => x(d.value)-x(0)-1)
+      .attr('y', d => y(top_n+1)+5)
+      .attr('height', y(1)-y(0)-bar_padding)
+      .style('fill', d => d.colour)
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('y', d => y(d.rank)+5);
           
-     bars
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('width', d => x(d.value)-x(0)-1)
-       .attr('y', d => y(d.rank)+5)
-       .attr('height', y(1)-y(0)-bar_padding);
-            
-     bars
-       .exit()
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('width', d => x(d.value)-x(0)-1)
-       .attr('y', d => y(top_n+1)+5)
-       .remove();
+    bars
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('width', d => x(d.value)-x(0)-1)
+      .attr('y', d => y(d.rank)+5)
+      .attr('height', y(1)-y(0)-bar_padding);
+          
+    bars
+      .exit()
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('width', d => x(d.value)-x(0)-1)
+      .attr('y', d => y(top_n+1)+5)
+      .remove();
 
-     let labels = svg.selectAll('.label').data(frameSlice, d => d.name);
+    let labels = svg.selectAll('.label').data(frame_slice, d => d.name);
      
-     labels
-       .enter()
-       .append('text')
-       .attr('class', 'label')
-       .attr('x', d => x(d.value)-8)
-       .attr('y', d => y(top_n+1)+5+((y(1)-y(0))/2))
-       .style('text-anchor', 'end')
-       .html(d => d.name)    
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
+    labels
+      .enter()
+      .append('text')
+      .attr('class', 'label')
+      .attr('x', d => x(d.value)-8)
+      .attr('y', d => y(top_n+1)+5+((y(1)-y(0))/2))
+      .style('text-anchor', 'end')
+      .html(d => d.name)    
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
     
-     labels
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('x', d => x(d.value)-8)
-       .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
+    labels
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('x', d => x(d.value)-8)
+      .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
      
-     labels
-       .exit()
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('x', d => x(d.value)-8)
-       .attr('y', d => y(top_n+1)+5)
-       .remove();
+    labels
+      .exit()
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('x', d => x(d.value)-8)
+      .attr('y', d => y(top_n+1)+5)
+      .remove();
      
-     let valueLabels = svg.selectAll('.valueLabel').data(frameSlice, d => d.name);
+    let value_labels = svg.selectAll('.value_label').data(frame_slice, d => d.name);
   
-     valueLabels
-       .enter()
-       .append('text')
-       .attr('class', 'valueLabel')
-       .attr('x', d => x(d.value)+5)
-       .attr('y', d => y(top_n+1)+5)
-       .text(d => d3.format(',.0f')(d.last_value))
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
+    value_labels
+      .enter()
+      .append('text')
+      .attr('class', 'value_label')
+      .attr('x', d => x(d.value)+5)
+      .attr('y', d => y(top_n+1)+5)
+      .text(d => d3.format(',.0f')(d.last_value))
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1);
        
-     valueLabels
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('x', d => x(d.value)+5)
-       .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1)
-       .tween("text", function(d) {
-         let i = d3.interpolateRound(d.last_value, d.value);
-         var node = this;
-         return function(t) {
-           node.textContent = d3.format(',')(i(t));
-         };
-       });
+    value_labels
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('x', d => x(d.value)+5)
+      .attr('y', d => y(d.rank)+5+((y(1)-y(0))/2)+1)
+      .tween("text", function(d) {
+        let i = d3.interpolateRound(d.last_value, d.value);
+        var node = this;
+        return function(t) {
+          node.textContent = d3.format(',')(i(t));
+        };
+      });
      
-     valueLabels
-       .exit()
-       .transition()
-       .duration(tick_duration)
-       .ease(d3.easeLinear)
-       .attr('x', d => x(d.value)+5)
-       .attr('y', d => y(top_n+1)+5)
-       .remove();
+    value_labels
+      .exit()
+      .transition()
+      .duration(tick_duration)
+      .ease(d3.easeLinear)
+      .attr('x', d => x(d.value)+5)
+      .attr('y', d => y(top_n+1)+5)
+      .remove();
     
-     frameText.html(~~frame);
-     
-     if(frame >= last_frame) ticker.stop();
+    //frame_text.html(~~frame); 
+    update_frame_lbl();
+    
+    if(frame >= last_frame) ticker.stop();
    
-     frame = d3.format('.1f')((+frame) + 0.1);
-   }, tick_duration, bar_padding);
- });
+    frame = frame + 1;
+  }, tick_duration, bar_padding);
+});
     
 const halo = function(text, strokeWidth) {
   text.select(function() { return this.parentNode.insertBefore(this.cloneNode(true), this); })
